17a81062f8f2771439f69a4e861b5d1951a86bc3
Jun Wako <wakojun@gmail.com>
2015-08-13 15:57:33 +0900

    Test for Topre capacitive sesing

---
+++ main.c
@@ -0,0 +1,100 @@
+#include <avr/cpufunc.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "common/wait.h"
+
+int main(void)
+{
+    /* Disable watchdog if enabled by bootloader/fuses */
+    MCUSR &= ~(1 << WDRF);
+    wdt_disable();
+
+    /* Disable clock division */
+    clock_prescale_set(clock_div_1);
+
+    // LED on
+    DDRD = (1<<6);
+    PORTD = (1<<6);
+
+    // burst signal
+    DDRC = 0xFF;
+    PORTC = 0x00;
+
+    // top: PF0
+    // bottom: PF1
+    // slope: PF2
+    DDRF = 0x00;
+    PORTF = 0x00;
+
+    cli();
+
+    for (;;) {
+        DDRC= 0xFF;
+        PORTC = 0x00;
+        DDRF = 0x00;
+        PORTF = 0x00;
+
+        for (int i = 0; i < 32; i++) {
+            DDRF &= ~(1<<0); PORTF &= ~(1<<0);   // top: HiZ
+            DDRF |=  (1<<1); PORTF &= ~(1<<1);   // bttom: Lo
+            //DDRF &= ~(1<<2); PORTF &= ~(1<<2);   // slope: HiZ
+
+            // rising edge
+            PORTC = 0x01;
+
+            _NOP(); _NOP(); _NOP(); _NOP(); _NOP(); // 1us cycle
+            //_delay_us(5);
+
+
+            DDRF &= ~(1<<1); PORTF &= ~(1<<1);   // bttom: HiZ
+            DDRF |=  (1<<0); PORTF &= ~(1<<0);   // top: Lo
+            //DDRF &= ~(1<<2); PORTF &= ~(1<<2);   // slope: HiZ
+
+            // falling edge
+            PORTC = 0x00;
+
+            _NOP();   // 1us cycle
+            //_delay_us(5);
+        }
+
+        // sense
+        DDRF |=  (1<<0); PORTF &= ~(1<<0);   // top: Lo 
+        DDRF &= ~(1<<1); PORTF &= ~(1<<1);   // bttom: HiZ
+        PORTF |=  (1<<2); DDRF |=  (1<<2);    // slope: Hi
+        _delay_us(300); //wait_us(100);
+
+
+        // discharge capacitor top:Lo, bottom: Lo
+        DDRF |=  (1<<0); PORTF &= ~(1<<0);   // top: Lo 
+        DDRF |=  (1<<1); PORTF &= ~(1<<1);   // bttom: Lo
+        _delay_us(10); //wait_us(100);
+
+/*
+        DDRC= 0x00;    PORTC = 0x00;
+
+        DDRF |=  (1<<0); PORTF &= ~(1<<0);   // top: Lo 
+        DDRF &= ~(1<<1); PORTF &= ~(1<<1);   // bttom: HiZ
+        PORTF |=  (1<<2); DDRF |=  (1<<2);    // slope: Hi
+        _delay_us(1000); //wait_us(100);
+
+        PORTF |= (1<<0) | (1<<1);
+        DDRF |=  (1<<2); PORTF &=  ~(1<<2);   // slope: Lo
+*/
+    }
+
+
+
+/* not swing with full voltage (around 3.0V)
+    //Input with pullup
+    DDRC = 0x00;
+    PORTC = 0xFF;
+
+    for (;;) {
+        PINC = 0xFF;
+        wait_us(10);
+    }
+*/
+}
